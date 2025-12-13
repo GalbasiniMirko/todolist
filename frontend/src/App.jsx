fc6a0e591b3910b31ab1bcbf281b88f5d22ba18b
Galba <galba@Galba.GalbaDomain>
2025-12-13 15:43:18 +0100

    feat(frontend): implement login API and implement local storage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
 
+      <Route path="/protected" element={<h1>Welcome to ToDoList!</h1>} />
+
       <Route path="*" element={<h1 className="text-center mt-10">404 Pagina non trovata</h1>} />
     </Routes>
   );
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,23 +1,79 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function LoginPage() {
+    const navigate = useNavigate();
     const [isShown, setIsShown] = useState(false);
+    
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
 
     const togglePassword = () => {
         setIsShown((isShown) => !isShown);
     }
 
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+
+        try {
+            const response = await fetch("/api/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: formData.email,
+                    password: formData.password
+                }),
+            });
+
+            const data = await response.json();
+
+            if (!response.ok) {
+                throw new Error(data.error || "Error during registration");
+            }
+
+            localStorage.setItem("token", data.accessToken);
+            localStorage.setItem("refreshToken", data.refreshToken);
+
+            alert("Successfull login!");
+            navigate("/protected");
+        } catch (err) {
+            setError(err.message);
+        }
+    };
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
             <div className="w-full max-w-sm bg-white shadow-xl rounded-2xl p-6">
                 <h2 className="text-3xl font-bold text-center mb-6">Login</h2>
-                <form className="flex flex-col gap-4">
+
+                {error && (
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded mb-4 text-sm text-center">
+                        {error}
+                    </div>
+                )}
+
+                <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
                     <div className="flex flex-col gap-1">
                         <label className="text-lg font-medium" htmlFor="email">Email</label>
                         <input
                             id="email"
                             type="email"
+                            value={formData.email}
+                            onChange={handleChange}
                             className="border rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Enter your email"
                         />
@@ -28,6 +84,8 @@ function LoginPage() {
                         <input
                             id="password"
                             type={isShown ? "text" : "password"}
+                            value={formData.password}
+                            onChange={handleChange}
                             className="border rounded-xl p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                             placeholder="Enter your password"
                         />
@@ -51,7 +109,7 @@ function LoginPage() {
                     </button>
 
                     <p className="text-center text-sm mt-3">
-                        Non hai un account?{" "}
+                        Don't have an account?{" "}
                         <Link to="/signup" className="text-blue-600 font-medium hover:underline">Sign up</Link>
                     </p>
                 </form>
